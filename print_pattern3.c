6bc3e476a8375fd3873d2948eaebda7af68ff3be
S. M. Redwan <smredwan.info@gmail.com>
2022-06-09 08:35:03 +0600

    commited after completely write pyramid code in patter3.c file

--- print_pattern3.c
+++ print_pattern3.c
@@ -1,1 +1,29 @@
-/* printing pattern      
+/* printing pattern make pyramid
+            *
+          * * *
+        * * * * *
+      * * * * * * * 
+      */
+
+#include <stdio.h>
+
+int main() {
+    int row, space, sym, row_no;
+
+    printf("Please enter how many do you want to print: ");
+    scanf("%d", &row_no);
+
+    //calculating row
+    for(row = 1; row <= row_no; row++) {
+        //printing space
+        for(space = 1; space <= (row_no - row); space++) {
+            printf(" ");
+        }
+        for(sym = 1; sym <= ((2*row) - 1); sym++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
