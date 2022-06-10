dd62cf271d1d55a990e5e487e169263bbe124c61
S. M. Redwan <smredwan.info@gmail.com>
2022-06-10 11:38:08 +0600

    Commiting after fixed the bugs in this program and added the features of multiplication

--- add_and_multiple_number_n_times.c
+++ add_and_multiple_number_n_times.c
@@ -3,20 +3,29 @@ If any input provide as negative the program will terminate.
 And if any input provide as character the program will skip those. */
 
 #include <stdio.h>
+#include <windows.h>
 
 int main() {
     int n, i;
-    double  num, sum;
+    double  num, sum, prod;
+    char ch;
 
-    printf("Please how many time you want to summition: ");
+    printf("Please enter how many time you want to summition: ");
     scanf("%d", &n);
+
+    system("cls");
+    printf("\n (if you input any character program will skip them \n and if you input any negative number then the program will terminate imidiately):\n\n");
+
 
     i = 1;
     sum = 0;
+    prod = 1;
 
     while (i <= n) {
-        printf("Enter %d(st, nd)th number to sum: ", i);
+        printf("Enter %d(st, nd)th number to summitin and multiplicatin: ", i);
         scanf(" %lf", &num);
+        scanf("%c", &ch);
+
 
         i++;
 
@@ -25,13 +34,15 @@ int main() {
             break;
         }
         //cheaking for character
-        if((char)num >= 'A' && (char)num <= 'Z' || (char)num >= 'a' && (char)num <= 'z') {
+        if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
             continue;
         }
         sum += num;
+        prod *= num;
     }
     
     printf("The summition of your providing number is: %.2lf\n", sum);
+    printf("The multiplication of your providing number is: %.2lf\n", prod);
 
 
     return 0;
